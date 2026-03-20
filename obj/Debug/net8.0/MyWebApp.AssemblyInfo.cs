d09cb96359c7f91d51916e9274cab6a8c4c52704
lejohndev <lejohn2801@gmail.com>
2026-03-20 12:54:09 +0700

    add sth design UI

--- obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329e5051f17e3970b6708ae973415e1367563628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f754cd8fe05800c6b15ecd7609a1012a2d53529")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
